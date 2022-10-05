4cb83ac9a08b2f3974a3b583cdb5e63a406d0c92
yyppsk <69805496+yyppsk@users.noreply.github.com>
2022-10-05 15:20:56 +0530

    Duplicate Using Efficient Algorithm

--- October22/Findduplicatesinanarray.cpp
+++ October22/Findduplicatesinanarray.cpp
@@ -31,10 +31,24 @@ vector<int> duplicates(int arr[], int n)
         return result;
     }
 }
+vector<int> duplicatessol2(int arr[], int n)
+{
+    vector<int> result;
+    for (int i = 0; i < n; i++)
+    {
+        int index = arr[i] % n;
+        arr[index] += n;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if ((arr[i] / n) >= 2)
+            result.push_back(i);
+    }
+    return result;
+}
 int main()
 {
-    int N = 5, a[] = {2, 3, 1, 2, 3};
-    duplicates(a, N);
-
+    int n = 5, arr[] = {4, 4, 4, 4, 4};
+    duplicatessol2(arr, n);
     return 0;
 }
