a83dd085648c7171b2b1757461cd2124d2dfc132
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-04-18 11:09:09 +0530

    Missing and Repeating

---
+++ April 2023/RearrangeArrayAlternately.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+// This function wants you to modify the given input
+// array and no need to return anything
+// arr: input array
+// n: size of array
+// Function to rearrange  the array elements alternately.
+void rearrange(long long *arr, int n)
+{
+
+    // Your code here
+}
+int main()
+{
+    return 0;
+}
