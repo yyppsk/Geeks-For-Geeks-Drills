d895b212fa8958b03b020ec61911bb6e4aabed55
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-04-18 11:23:07 +0530

    RearrangeArrayAlternate

--- April 2023/RearrangeArrayAlternately.cpp
+++ April 2023/RearrangeArrayAlternately.cpp
@@ -10,10 +10,44 @@ using namespace std;
 // Function to rearrange  the array elements alternately.
 void rearrange(long long *arr, int n)
 {
-
-    // Your code here
+    vector<long long> extra;
+    long long start = 0, end = n - 1;
+    if (n % 2 != 0)
+        while (start <= end)
+        {
+            extra.push_back(arr[end]);
+            if (start < end)
+                extra.push_back(arr[start]);
+            start++, end--;
+        }
+    else
+        while (start < end)
+        {
+            extra.push_back(arr[end]);
+            extra.push_back(arr[start]);
+            start++, end--;
+        }
+    for (auto i : extra)
+    {
+        cout << i << " ";
+    }
+    int k = 0;
+    for (auto i : extra)
+    {
+        arr[k] = i;
+        k++;
+    }
+    cout << endl;
 }
 int main()
 {
+    int n = 11;
+    long long arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110};
+    rearrange(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i]
+             << " ";
+    }
     return 0;
 }
