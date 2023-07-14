8c1f5b6890a5f9c08d0cb0b1b485164b40509270
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-07-15 00:01:11 +0530

    boolean Matrix

--- July23/booleanMatrix.cpp
+++ July23/booleanMatrix.cpp
@@ -2,25 +2,58 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <set>
 using namespace std;
 void booleanMatrix(vector<vector<int>> &matrix)
 {
-    vector<int> targetRows, targetCols;
+    set<int> targetRows, targetCols;
     for (int i = 0; i < matrix.size(); i++)
     {
         for (int j = 0; j < matrix[i].size(); j++)
         {
             if (matrix[i][j] == 1)
             {
-                targetCols.push_back(j);
-                targetRows.push_back(i);
+                targetCols.insert(j);
+                targetRows.insert(i);
             }
         }
     }
+    /*
     for (auto const &ele : targetRows)
     {
         cout << ele << " ";
     }
+    cout << endl;
+    for (auto const &ele : targetCols)
+    {
+        cout << ele << " ";
+    }
+    */
+    for (auto const &ele : targetCols)
+    {
+        for (int k = 0; k < matrix.size(); k++)
+        {
+            matrix[k][ele] = 1;
+        }
+    }
+    for (auto const &ele : targetRows)
+    {
+        for (int k = 0; k < matrix[0].size(); k++)
+        {
+            matrix[ele][k] = 1;
+        }
+    }
+    /*
+    cout << endl;
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        for (int j = 0; j < matrix[i].size(); j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    */
 }
 int main()
 {
